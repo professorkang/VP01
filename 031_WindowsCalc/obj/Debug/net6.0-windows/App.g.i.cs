84b615a3f2217908e76a904d7c966faa06de3748
professorkang <kquddlr@naver.com>
2022-04-20 04:44:09 +0900

    홈에서 연결

--- 031_WindowsCalc/obj/Debug/net6.0-windows/App.g.i.cs
+++ 031_WindowsCalc/obj/Debug/net6.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace _031_WindowsCalc {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace _031_WindowsCalc {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
         public static void Main() {
             _031_WindowsCalc.App app = new _031_WindowsCalc.App();
             app.InitializeComponent();
