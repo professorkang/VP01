a2c16beb6b7b127241ba66dc6dbb0d88473ebd2c
professorkang <1@1.1>
2022-04-28 15:50:12 +0900

    수업

---
+++ 032_ListNArray/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+List<int> lst = new List<int>();
+int[] arr = new int[10];
+double[] arrd = new double[10];
+
+Random r = new Random();
+for (int i = 0; i < 10; i++)
+{
+  arrd[i] = r.NextDouble(); // 0~1미만
+  arr[i] = r.Next(100);
+  lst.Add(arr[i]);
+}
+
+Print(arrd);
+Print(arr);
+PrintList(lst);
+
+lst.Sort();
+Array.Sort(arr);
+Array.Sort(arrd);
+
+Console.WriteLine("\nAfter Sort");
+Print(arrd);
+Print(arr);
+PrintList(lst);
+
+void PrintList(List<int> lst)
+{
+  foreach(var i in lst)
+    Console.Write(i + " ");
+  Console.WriteLine();
+}
+
+void Print<T>(T[] a)
+{
+  foreach(var item in a)
+    Console.Write("{0:F2} ", item);
+  Console.WriteLine();
+}
---
+++ 033_PhoneBook/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\807PC02\Documents\StudentDB.accdb
+namespace _033_PhoneBook
+{
+  public partial class Form1 : Form
+  {
+    string connStr = @"Provider = Microsoft.ACE.OLEDB.12.0; Data Source = C:\Users\807PC02\Documents\StudentDB.accdb";
+    OleDbConnection conn = null;
+    OleDbCommand comm = null;
+    OleDbDataReader reader = null;
+    public Form1()
+    {
+      InitializeComponent();
+      DisplayStudents();
+    }
+
+    // 교과서 602쪽
+    private void DisplayStudents()
+    {
+      if(conn == null)
+      {
+        conn = new OleDbConnection(connStr);
+        conn.Open();
+      }
+
+      string sql = "SELECT * FROM StudentTable";
+      comm = new OleDbCommand(sql, conn);
+      reader = comm.ExecuteReader();
+      while(reader.Read())
+      {
+        string x = "";
+        x += reader["ID"] + "\t";
+        x += reader["SId"] + "\t";
+        x += reader["SName"] + "\t";
+        x += reader["Phone"];
+        lstStudent.Items.Add(x);
+      }
+      reader.Close();
+      conn.Close();
+      conn = null;
+    }
+  }
+}
