bc9a3f74c57f60e875a87015ee8f0b1837df7793
professorkang <103405363+professorkang@users.noreply.github.com>
2022-05-11 15:04:52 +0900

    Add files via upload

---
+++ 035_Splash/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _035_Splash
+{
+  /// <summary>
+  /// MainWindow.xaml에 대한 상호 작용 논리
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void btnRandom_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void btnDB_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void btnReset_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void btnExit_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+  }
+}
