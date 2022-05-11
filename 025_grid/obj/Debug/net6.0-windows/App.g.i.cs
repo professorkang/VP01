eead102284aa30fd7f3a4f35e06538f191da9335
professorkang <kquddlr@naver.com>
2022-05-11 15:48:37 +0900

    연구실

--- 025_grid/obj/Debug/net6.0-windows/App.g.i.cs
+++ 025_grid/obj/Debug/net6.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace _025_grid {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.3.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace _025_grid {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.3.0")]
         public static void Main() {
             _025_grid.App app = new _025_grid.App();
             app.InitializeComponent();
--- 027_stackPanel/obj/Debug/net6.0-windows/App.g.i.cs
+++ 027_stackPanel/obj/Debug/net6.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace _027_stackPanel {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.3.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace _027_stackPanel {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.3.0")]
         public static void Main() {
             _027_stackPanel.App app = new _027_stackPanel.App();
             app.InitializeComponent();
